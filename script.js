600f3fd40deccd0d698ac97aaaebe60bd2cea0e7
gerardolq <58379792+gerardolq@users.noreply.github.com>
2020-01-02 16:48:14 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//write stuff
+function restartHover(btn){
+	var name;
+	if(btn == "pvpbtn"){
+		name = "LandingImages/pvphover.png"
+	}
+	else{
+		name = "LandingImages/pvAIhover.png"
+	}
+	document.getElementById(btn).src = name;
+}
+
+function resetHover(btn){
+	var name;
+	if(btn == "pvpbtn"){
+		name = "LandingImages/pvp.png"
+	}
+	else{
+		name = "LandingImages/pvAI.png"
+	}
+	document.getElementById(btn).src = name;
+}
+
